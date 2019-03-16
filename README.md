35116a57b8a76ef12d736f2e78aebbf9f06da8cd
Martin Splitt <martinsplitt@google.com>
2019-03-16 16:24:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+Fix SPA SEO codelab
+============
+
+This codelab walks you through a single page app (SPA) with SEO issues and illustrates how to identify and fix these issues. You will learn more about debugging tools, SEO tips and important considerations. 
+
+Introduction
+------------
+Google Search works hard to understand the content of a web page. 
+
+You can help by testing your pages with the tools provided by Google and following a few guidelines and general principles.
+
+Pre-requisites
+--------------
+
+- A recent version of Chrome or other modern web browser
+- The sample code
+- A text editor (This is optional. You can use glitch as well) 
+- Basic understanding of HTML and JavaScript syntax and features.
+
+Support
+-------
+
+You can ask general questions or help others with their JS-related issues at https://bit.ly/js-sites-wg
+
+If you've found an error in this sample, please file an issue:
+https://github.com/googlecodelabs/fix-spa-seo/issues
+
+Patches are encouraged, and may be submitted by forking this project and
+submitting a pull request through GitHub.
+
+License
+-------
+
+Copyright 2018 Google, Inc.
+
+Licensed to the Apache Software Foundation (ASF) under one or more contributor
+license agreements.  See the NOTICE file distributed with this work for
+additional information regarding copyright ownership.  The ASF licenses this
+file to you under the Apache License, Version 2.0 (the "License"); you may not
+use this file except in compliance with the License.  You may obtain a copy of
+the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+License for the specific language governing permissions and limitations under
+the License.
---
+++ app.js
@@ -0,0 +1,95 @@
+/*
+    Copyright 2019 Google LLC
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+* @fileoverview The JavaScript for our single page app.
+*/
+
+"use strict";
+
+// the templates & the target container for the view
+const itemTemplate = document.querySelector('template#story-item');
+const errorTemplate = document.querySelector('template#error');
+const listContainer = document.querySelector('#content-container');
+const notificationsContainer = document.querySelector('#notifications-container');
+const heading = document.querySelector('h1');
+
+// whenever the hash of the URL changes, we load the view for the new hash
+window.addEventListener('hashchange', evt => {
+  let category = window.location.hash.slice(1); // removes the leading '#'
+  // if the category is empty, show the_keyword as the homepage.
+  if (category == '') category = 'the_keyword';
+  showStoriesForCategory(category);
+});
+
+// Load the content for the current hash
+let category = window.location.hash.slice(1); // we remove the leading '#'
+// if the category is empty, show the_keyword as the homepage.
+if (category == '') category = 'the_keyword';
+showStoriesForCategory(category);
+
+// Load the Material Component code for the navigation drawer menu
+const menu = mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+// Make the menu button toggle the navigation menu
+document.getElementById('menuToggle').addEventListener('click', () => {
+  menu.open = !menu.open;
+});
+
+// Functions
+
+/**
+ * Loads the articles for the given category,
+ * clears the target container and adds the articles as cards to the container.
+ * @param {string} category the category to load stories for
+*/
+function showStoriesForCategory(category) {
+  // load the stories from the JSON file of the desired category
+  // Note: in a real application this would be an API with the latest stories
+  // instead of a static file.
+  fetch(`/data/${category}.json`)
+  .then(response => response.json())
+  .then(stories => {
+    // remove the previous view content
+    notificationsContainer.innerHTML = '';
+    listContainer.innerHTML = '';
+    // update the heading
+    heading.textContent = stories[0].category;
+
+    return stories.map(story => {
+      // load the template for an article card
+      let item = document.importNode(itemTemplate.content, true);
+      // fill in the data
+      item.querySelector('h2').textContent = story.title;
+      item.querySelector('h3').textContent = `by ${story.author}`;
+      item.querySelector('img').src = `/data/${story.imgSrc}`;
+      item.querySelector('.summary').textContent = story.summary;
+      item.querySelector('button').addEventListener('click', () => {
+        location.href = story.link;
+      });
+      return item;
+    });
+  })
+  .then(cards => {
+    // add the article card to the view
+    cards.forEach(card => listContainer.appendChild(card));
+  }).catch(e => {
+    notificationsContainer.innerHTML = '';
+    listContainer.innerHTML = '';
+    // load and display the error message template
+    let errorMsg = document.importNode(errorTemplate.content, true);
+    notificationsContainer.appendChild(errorMsg);
+  });
+}
